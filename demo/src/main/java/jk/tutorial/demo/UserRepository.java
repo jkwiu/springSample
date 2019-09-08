b7a3e09d819232f77fb1ff179963cffb511a5f9d
sizzflyer <sizzflyer@gmail.com>
2019-09-09 01:51:54 +0900

    continue

---
+++ demo/src/main/java/jk/tutorial/demo/UserRepository.java
@@ -0,0 +1,12 @@
+package jk.tutorial.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import jk.tutorial.demo.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
